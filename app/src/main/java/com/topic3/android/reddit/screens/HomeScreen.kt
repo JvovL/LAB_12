1db1b85182673df97c4f2492549431a013afc53c
JvovL <jjvovo715@gmail.com>
2024-04-03 15:12:00 +0300

    Добавление TrendingTopics на главный экран 2-3

--- app/src/main/java/com/topic3/android/reddit/screens/HomeScreen.kt
+++ app/src/main/java/com/topic3/android/reddit/screens/HomeScreen.kt
@@ -83,22 +83,43 @@ fun HomeScreen(viewModel: MainViewModel) {
             Timer().schedule(3000) {isToastVisible = false}
         }
     }
+
+    val homeScreenItems = mapHomeScreenItems(posts)
+
     Box(modifier = Modifier.fillMaxSize()) {
         LazyColumn(
-            modifier = Modifier.background(
-                color =
-                MaterialTheme.colors.secondary
-            )
-        ){
-            items(posts){
-                if (it.type == PostType.TEXT){
-                    TextPost(it, onJoinButtonClick = onJoinClickAction)
-                } else {
-                    ImagePost(it, onJoinButtonClick = onJoinClickAction)
-                }
-                Spacer(modifier = Modifier.height(6.dp))
+            modifier = Modifier
+                .background(color = MaterialTheme.colors.secondary),
+            content = {
+                items(
+                    items = homeScreenItems,
+                    itemContent = { item ->
+                        if (item.type == HomeScreenItemType.TREDING){
+                            TrendingTopics(
+                                trendingTopics = trendingItems,
+                                modifier = Modifier.padding(
+                                    top = 16.dp, bottom = 6.dp
+                                )
+                            )
+                        } else if (item.post != null){
+                            val post = item.post
+                            if (post.type == PostType.TEXT){
+                                TextPost(
+                                    post = post,
+                                    onJoinButtonClick = onJoinClickAction
+                                )
+                            } else {
+                                ImagePost(
+                                    post = post,
+                                    onJoinButtonClick = onJoinClickAction
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(6.dp))
+                        }
+                    }
+                )
             }
-        }
+        )
         Box(
             modifier = Modifier
                 .align(Alignment.BottomCenter)
@@ -107,6 +128,23 @@ fun HomeScreen(viewModel: MainViewModel) {
             JoinedToast(visible = isToastVisible)
         }
     }
+}
+
+private fun mapHomeScreenItems(
+    posts: List<PostModel>
+): List<HomeScreenItem> {
+    val homeScreenItems = mutableListOf<HomeScreenItem>()
+
+    homeScreenItems.add(
+        HomeScreenItem(HomeScreenItemType.TREDING)
+    )
+
+    posts.forEach {post ->
+        homeScreenItems.add(
+            HomeScreenItem(HomeScreenItemType.POST, post)
+        )
+    }
+    return homeScreenItems
 }
 
 private data class HomeScreenItem(
